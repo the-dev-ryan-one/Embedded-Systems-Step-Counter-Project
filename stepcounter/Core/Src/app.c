9ee2e510522c6a741329c01b78939d6f1b318ee6
unknown <rte26@uclive.ac.nz>
2026-03-13 14:38:35 +1300

    joystick for ms1 working

--- stepcounter/Core/Src/app.c
+++ stepcounter/Core/Src/app.c
@@ -47,10 +47,33 @@ void display_task_execute(void) {
     char buffer[32];
 
     uint16_t x = getJoyStickX();
+    uint16_t maxXValue = 3850;
+    uint16_t minXValue = 470;
+
+    uint16_t percentageXdisplacement = ((x - minXValue) * 100) / (maxXValue - minXValue);
+    if (percentageXdisplacement > 100) {
+    	percentageXdisplacement = 100;
+    }
+
+    char* xJoyDirection;
+
+    if (percentageXdisplacement > 60) {
+
+    	xJoyDirection = "Left";
+
+
+    } else if (percentageXdisplacement < 40) {
+    	xJoyDirection = "Right";
+    } else {
+    	xJoyDirection = "Rest";
+    }
+
+
+
 
     ssd1306_Fill(Black);
     ssd1306_SetCursor(0, 0);
-    snprintf(buffer, sizeof(buffer), "Joy X value: %u", x);
+    snprintf(buffer, sizeof(buffer), "Joy X : %s %u", xJoyDirection , percentageXdisplacement);
     ssd1306_WriteString(buffer, Font_7x10, White);
 
     if (serialDebugFlag) {
@@ -61,9 +84,27 @@ void display_task_execute(void) {
     }
 
     uint16_t y = getJoyStickY();
+    uint16_t maxYValue = 3900;
+    uint16_t minYValue = 335;
+
+    uint16_t percentageYdisplacement = ((y - minYValue) * 100) / (maxYValue - minYValue);
+    if (percentageYdisplacement > 100) {
+        	percentageYdisplacement = 100;
+        }
+
+    char* yJoyDirection;
+
+    if (percentageYdisplacement > 60) {
+        yJoyDirection = "Down";
+    } else if (percentageYdisplacement < 40) {
+        yJoyDirection = "Up";
+    } else {
+        yJoyDirection = "Rest";
+    }
+
 
     ssd1306_SetCursor(0, 16);
-    snprintf(buffer, sizeof(buffer), "Joy Y value: %u", y);
+    snprintf(buffer, sizeof(buffer), "Joy Y : %s %u", yJoyDirection , percentageYdisplacement);
 
     ssd1306_WriteString(buffer, Font_7x10, White);
     ssd1306_UpdateScreen();
