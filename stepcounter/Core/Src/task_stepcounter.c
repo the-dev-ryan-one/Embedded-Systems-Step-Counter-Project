50be186d1c9448cc44b0015c06b14d6464ad6531
rte26 <rte26@uclive.ac.nz>
2026-04-24 12:48:58 +1200

    trying to get step counter interrupt to fire , not working

--- stepcounter/Core/Src/task_stepcounter.c
+++ stepcounter/Core/Src/task_stepcounter.c
@@ -27,7 +27,7 @@ void HAL_GPIO_EXTI_Rising_Callback(uint16_t GPIO_Pin)
 	snprintf(buffer, sizeof(buffer), "EXTI fired: pin %d\r\n", GPIO_Pin);
 	HAL_UART_Transmit(&huart2, (uint8_t*)buffer, strlen(buffer), 100);
 
-	if (GPIO_Pin & GPIO_PIN_10)
+	if (GPIO_Pin && GPIO_PIN_10)
 	{
 		steps++;
 	}
