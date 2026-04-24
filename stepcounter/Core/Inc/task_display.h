3b088332075633515106d11a13ec35921add20fa
rte26 <rte26@uclive.ac.nz>
2026-04-24 15:44:49 +1200

    step counter all good , ready to merge with main

--- stepcounter/Core/Inc/task_display.h
+++ stepcounter/Core/Inc/task_display.h
@@ -1,17 +1,12 @@
-#ifndef TASK_DISPLAY_H
-#define TASK_DISPLAY_H
-
-//extern uint16_t steps;
-//extern uint16_t distance;
-//extern uint16_t stepGoal;
-
-
 /*
  * task_display.h
  * Header file for the display task module
  * Authors: Ryan Teape, Felissa Chian
  * Date: 2026
  */
+
+#ifndef TASK_DISPLAY_H
+#define TASK_DISPLAY_H
 
 /* updates OLED display */
 void display_task_execute(void);
--- stepcounter/Core/Src/task_stepcounter.c
+++ stepcounter/Core/Src/task_stepcounter.c
@@ -4,39 +4,22 @@
  *  Created on: 14/04/2026
  *      Author: Uni
  */
+
 #include "task_stepcounter.h"
 #include "accelerometer.h"
 #include "imu_lsm6ds.h"
 #include "app.h"
 #include "usart.h"
 
-//void HAL_GPIO_EXTI_Rising_Callback(uint16_t GPIO_Pin) {
-//
-//	char buffer[32];
-//	snprintf(buffer, sizeof(buffer), "EXTI fired: pin %d\r\n", GPIO_Pin);
-//	HAL_UART_Transmit(&huart2, (uint8_t*)buffer, strlen(buffer), 100);
-//
-//    if (GPIO_Pin == GPIO_PIN_10) {
-//        steps = getStepCount();
-//    }
-//}
 
 #include <stdbool.h>
 
 void HAL_GPIO_EXTI_Rising_Callback(uint16_t GPIO_Pin)
 {
-
-//	char buffer[32];
-//	snprintf(buffer, sizeof(buffer), "EXTI fired: pin %d\r\n", GPIO_Pin);
-//	HAL_UART_Transmit(&huart2, (uint8_t*)buffer, strlen(buffer), 100);
-
 	if (GPIO_Pin == GPIO_PIN_10)
 	{
-
-//		clearInteruptFlag = true;
 		steps++;
 	}
-
 }
 
 
