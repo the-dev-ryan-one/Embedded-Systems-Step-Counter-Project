30e5e39deb0f64f7b087696f3fdaa864c9dfbba2
fch67 <fch67@uclive.ac.nz>
2026-04-30 14:34:37 +1200

    added commetns and file header to potentiometer.c

--- stepcounter/Core/Src/potentiometer.c
+++ stepcounter/Core/Src/potentiometer.c
@@ -1,9 +1,10 @@
 /*
  * potentiometer.c
- *
- *  Created on: 27/04/2026
- *      Author: Uni
+ * Maps potentiometer ADC readings to a step goal within the max and min range rounded to the nearest goal increment size
+ * Authors: Ryan Teape, Felissa Chian
+ * Date: 27/04/2026
  */
+
 #include <stdbool.h>
 #include "adc.h"
 #include "app.h"
@@ -18,6 +19,7 @@
 
 static uint16_t rawPotVal;
 
+// Returns the raw ADC reading from the potentiometer channel
 static uint16_t getRawPotentiometerVal  (void)
 {
 	return raw_adc[ADC_INDEX_POT];
@@ -27,15 +29,18 @@ void potentiometer_task(void)
 {
 	rawPotVal = getRawPotentiometerVal();
 
+	// Clamp low ADC values to minimum goal to avoid jitter near zero
 	if (rawPotVal < RAW_POT_MIN)
 	{
 		newGoal = MIN_GOAL;
 	}
 	else
 	{
+		// Linearly scale the ADC reading to the goal range
 		newGoal = (float)(rawPotVal - RAW_POT_MIN) / (MAX_ADC - RAW_POT_MIN) * (MAX_GOAL - MIN_GOAL) + MIN_GOAL;
 	}
 
+	// Round down to the nearest increment for cleaner goal values
 	newGoal = (newGoal/GOAL_INCREMENT_SIZE)*GOAL_INCREMENT_SIZE;
 
 }
