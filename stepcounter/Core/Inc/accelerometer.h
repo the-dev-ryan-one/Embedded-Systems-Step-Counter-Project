5569f4d68db88017c7d850b7360f45039d38dcca
fch67 <fch67@uclive.ac.nz>
2026-04-30 14:30:04 +1200

    added file header to accelerometer.h

--- stepcounter/Core/Inc/accelerometer.h
+++ stepcounter/Core/Inc/accelerometer.h
@@ -1,3 +1,10 @@
+/*
+ * accelerometer.h
+ * Header for IMU initialisation using the LSM6DS sensor
+ * Authors: Ryan Teape, Felissa Chian
+ * Date: 14/04/2026
+ */
+ 
 #ifndef ACCELEROMETER_H
 #define ACCELEROMETER_H
 
