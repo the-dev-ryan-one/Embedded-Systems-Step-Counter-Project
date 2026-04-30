3d1c501936339bb20896a3a30074c6eca0633153
fch67 <fch67@uclive.ac.nz>
2026-04-30 14:36:24 +1200

    added comments and file header to potentiometer.h

--- stepcounter/Core/Inc/potentiometer.h
+++ stepcounter/Core/Inc/potentiometer.h
@@ -1,13 +1,14 @@
 /*
  * potentiometer.h
- *
- *  Created on: 28/04/2026
- *      Author: Uni
+ * Header for potentiometer task that maps ADC readings to a step goal value
+ * Authors: Ryan Teape, Felissa Chian
+ * Date: 28/04/2026
  */
 
 #ifndef INC_POTENTIOMETER_H_
 #define INC_POTENTIOMETER_H_
 
+// Reads the potentiometer ADC value and updates the current step goal
 void potentiometer_task(void);
 
 #endif /* INC_POTENTIOMETER_H_ */
